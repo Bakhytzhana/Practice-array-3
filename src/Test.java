73fef8fd6d50fa6ab39975bce12f4af5fe3d218f
Abeldinov Bakhytzhan <bakhytzhan79@mail.ru>
2021-09-13 14:56:29 +0600

    practice

---
+++ src/Test.java
@@ -0,0 +1,8 @@
+public class Test {
+    public static void main(String[] args) {
+        int[] nums = {7, -3, 9, -11, 18, 99, 2, 11};
+        for (int i = nums.length / 2; i < nums.length; i++) {
+            System.out.println(nums[i]);
+        }
+    }
+}
